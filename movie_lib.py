72e0d334da853944864c4e038614423a96578d09
gcrowder <gregory.j.crowder@gmail.com>
2016-04-07 14:51:20 -0400

    Started the Movie class. It takes a movie_id, movie_title, and a release_date.

---
+++ movie_lib.py
@@ -0,0 +1,5 @@
+class Movie:
+    def __init__(self, movie_id, movie_title, release_date):
+        self.movie_id = movie_id
+        self.movie_title = movie_title
+        self. release_date = release_date

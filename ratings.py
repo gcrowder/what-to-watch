f6e4b1b16c3cc4e2f3acf1a042e15ced72e64b51
gcrowder <gregory.j.crowder@gmail.com>
2016-04-08 15:16:18 -0400

    refactored all previous function calls into prepare_ratings().

--- ratings.py
+++ ratings.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime, date
+from datetime import datetime
 from movie_lib import Movie, User, Rating
 
 
@@ -25,19 +25,29 @@ def get_users():
         for row in reader:
             user_id = int(row['user_id'])
             movie_id = int(row['movie_id'])
-            rating = int(row['rating']
+            rating = int(row['rating'])
             if user_id in users:
                 users[user_id].add_rating(user_id, movie_id, rating)
             else:
                 users.update({user_id: User(user_id, movie_id, rating)})
     return users
 
+def make_movie_ratings(movies, users):
+    for movie_id, movie in movies.items():
+        for user_id, user in users.items():
+            if movie_id in user.ratings:
+                movies[movie_id].add_rating(movie_id, user_id, user.ratings[movie_id])
+    return movies
+
+def prepare_ratings():
+    movies = get_movies()
+    users = get_users()
+    movies = make_movie_ratings(movies, users)
+    all_ratings = Rating(movies, users)
+    return all_ratings
 
 def main():
-    movies = get_movies()
-    # for movie_id, movie in movies.items():
-    #     print("Movie ID: ", movie.movie_id, "Movie Title: ", movie.movie_title)
-    users = get_users()
+    movie_user_ratings = prepare_ratings()
 
 if __name__ == '__main__':
     main()

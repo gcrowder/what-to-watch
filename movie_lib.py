80ec8654f2abef5fd6268d255932996ba5dc0c8c
gcrowder <gregory.j.crowder@gmail.com>
2016-04-08 00:07:11 -0400

    Wrote all three classes, and all four methods required by the assignment. Added __pycache__ to .gitignore.

--- movie_lib.py
+++ movie_lib.py
@@ -1,18 +1,33 @@
 class Movie:
-    def __init__(self, movie_id, movie_title, release_date):
+    def __init__(self, movie_id, movie_title, release_date, user_id=0, rating=0):
         self.movie_id = movie_id
         self.movie_title = movie_title
-        self. release_date = release_date
+        self.release_date = release_date
+        self.ratings = {user_id : rating}
 
 class User:
     def __init__(self, user_id, movie_id, rating):
         self.user_id = user_id
-        self.movie_id = movie_id
-        self.rating = rating
+        self.ratings = {movie_id : rating}
 
 
 class Rating:
-    def __init__(self, user_id=0, movie_id=0, rating=0):
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self. rating = rating
+    def __init__(self, movies, users):
+        self.movies = movies #{movie_id : movie_object}
+        self.users = users #{user_id : user object}
+
+    def average_rating(movie_id):
+        movie = self.movies[movie_id]
+        return (sum(movie.ratings.values)) / len(movie.ratings)
+
+    def users_ratings(user_id):
+        user = self.users[user_id]
+        return user.ratings
+
+    def movies_ratings(movie_id):
+        movie = self.movies[movie_id]
+        return movie.ratings
+
+    def get_movie_title(movie_id):
+        movie = self.movies[movie_id]
+        return movie.title
--- movie_lib_tests.py
+++ movie_lib_tests.py
@@ -1,9 +1,26 @@
 import unittest
 from movie_lib import Movie, User, Rating
 
-test_rating = Rating(movie_id=1, rating=4, user_id=308)
 
-class TestRating:
 
-    def test_assert_equal_add_rating(self, rating, user_id, movie_id):
-        self.assertEqual(test_rating.get_rating(308), 4)
+
+
+
+
+
+
+# class TestRating(unittest.TestCase):
+#
+#     def test_assert_true_get_rating(self):
+#         test_rating = Rating(movie_id=1, rating=4, user_id=308)
+#         self.assertTrue(test_rating.get_rating(308) == 4)
+#
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

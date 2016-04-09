21faa9b76a306d16cf225dc2299e180470747fff
gcrowder <gregory.j.crowder@gmail.com>
2016-04-08 23:34:03 -0400

    Completed step three of assignment. most_popular_movies returns a list of the most popular movies with at least 10 ratings. top_rated_unseen_movies returns a list of most popular movies unseen by a given user.

--- ratings.py
+++ ratings.py
@@ -1,6 +1,8 @@
 import csv
+import operator
 from datetime import datetime
 from movie_lib import Movie, User, Rating
+import random
 
 
 def format_time(date):
@@ -46,8 +48,42 @@ def prepare_ratings():
     all_ratings = Rating(movies, users)
     return all_ratings
 
+def most_popular_movies(ratings_object):
+    averaged_movie_ratings = []
+    for movie_id in ratings_object.movies.keys():
+        if len(ratings_object.movies[movie_id].ratings) > 9:
+            averaged_movie_ratings.append((movie_id, ratings_object.average_rating(movie_id)))
+    descending_averaged_movie_ratings = sorted(averaged_movie_ratings, key=operator.itemgetter(1), reverse=True)
+    return descending_averaged_movie_ratings
+
+def show_movies(ratings_object, top_movies):
+    count = 0
+    for movie_id, rating in top_movies:
+        count += 1
+        if count < 21:
+            print("{}. {}: {}".format(count, ratings_object.movies[movie_id].movie_title, round(rating, 2)))
+
+def top_rated_unseen_movies(ratings_object, user_id):
+    averaged_movie_ratings = []
+    for movie_id in ratings_object.movies.keys():
+        if len(ratings_object.movies[movie_id].ratings) > 9:
+            if user_id not in ratings_object.movies[movie_id].ratings:
+                averaged_movie_ratings.append((movie_id, ratings_object.average_rating(movie_id)))
+    descending_averaged_movie_ratings = sorted(averaged_movie_ratings, key=operator.itemgetter(1), reverse=True)
+    return descending_averaged_movie_ratings
+
 def main():
     movie_user_ratings = prepare_ratings()
+    pop_movies = most_popular_movies(movie_user_ratings)
+    user_id = random.randint(1,943)
+    recommended_movies = top_rated_unseen_movies(movie_user_ratings, user_id)
+    print("Top 20 Films Overall: ")
+    show_movies(movie_user_ratings, pop_movies)
+    print("Top 20 Films Recommended for User: {}".format(user_id))
+    show_movies(movie_user_ratings, recommended_movies)
+
+
+
 
 if __name__ == '__main__':
     main()
